9dd2a852249b3e6a28cdc504b331318847c49000
Leckel15 <Lwaheckel@gmail.com>
2016-10-13 11:04:16 -0400

    Stroy Finished 2

--- story1.py
+++ story1.py
@@ -7,13 +7,13 @@ def story():
     monster1 = getWord("Enter a Monster: ")
     location2 = getWord("Enter a Location: ")
     action1 = getWord("Enter a Action: ")
-    adjective1 = getWord("Enter a Adjective: ")
+    noun1 = getWord("Enter a noun: ")
     adjective2 = getWord("Enter a Adjective: ")
     verb1 = getWord("Enter a Verb: ")
     adjective3 = getWord("Enter a Adjective: ")
-    adjective4 = getWord("Enter a Adjective: ")
-    action2 + getWord("Enter a Action: ")
-    object2 + getWord("Enter a Object: ")
+    noun4 = getWord("Enter a Plural Noun: ")
+    action2 = getWord("Enter a Action ending in ed: ")
+    object2 = getWord("Enter a Object: ")
     
     text = ""
     text += "One day Kent Heckel, the famous YouTuber was going for a walk in the " + location1
@@ -22,14 +22,14 @@ def story():
     text += "Before he could blink, Kasu add been dragged away by a little " + monster1 
     text += """. He started to yell, "Help me! He is taking me to his """ + location2
     text += """. He is going to """ + action1
-    text += ". Out of nowhere, Elijah the Magical " + adjective1 
+    text += ". Out of nowhere, Elijah the Magical " + noun1 
     text += " jumped out of the big " + adjective2
     text += " tree. He lands on top of the monster and starts to " + verb1
     text += """ Kent watches all of them fight and thinks "Hey i'll vlog this."""
     text += " At that moment all of the people fighting stop and look at him."
     text += " Kasu looks at the monster, then at Elijah and makes his " + adjective3
-    text += """ Kasu says, "Lets team up and kill him for trying to make dumb views off of us fighting. He is going to only get like 300 anyways."""
-    text += " So they all teamed up like a bunch of " + adjective4
+    text += """ face. Kasu says, "Lets team up and kill him for trying to make dumb views off of us fighting. He is going to only get like 300 anyways."""
+    text += " So they all teamed up like a bunch of " + noun4
     text += " and " + action2
     text += " him with a golden pated " + object2
     text += ". And he bled to death on the spot, and they took his stuff."
